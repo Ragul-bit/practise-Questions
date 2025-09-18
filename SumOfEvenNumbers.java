a5d68eb55a8580ead059ab10b0c9fb342ee25b00
Ragul-bit <ragul2005badge@gmail.com>
2025-09-18 23:25:25 +0530

    Update SumOfEvenNumbers.java

--- SumOfEvenNumbers.java
+++ SumOfEvenNumbers.java
@@ -1,21 +1,14 @@
 import java.util.Scanner;
-
 public class SumOfEvenNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Input: upper limit
         int n = sc.nextInt();
         int sum = 0;
-
-        // Add even numbers from 1 to n
         for (int i = 2; i <= n; i += 2) {
             sum += i;
         }
-
-        // Output
         System.out.println(sum);
-
         sc.close();
     }
 }
+
